3d177bb260d54f9082e4be413144abe3bd09aaba
Meowu <474384902@qq.com>
2019-08-10 23:26:16 +0800

    match strings start at any place of the text.

--- packages/basic/src/index.ts
+++ packages/basic/src/index.ts
@@ -12,3 +12,14 @@ export const match = (pattern: string, text: string): boolean => {
   if (pattern === '$' && text === '') return true;
   return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1))
 };
+
+export const search = (pattern: string, text: string): boolean => {
+  if (pattern[0] === '^') {
+    return match(pattern.slice(1), text)
+  } else {
+    // TODO: refactor this to be more efficient.
+    return text.split('').some((_: string, index: number) => {
+      return match(pattern, text.slice(index))
+    })
+  }
+};
