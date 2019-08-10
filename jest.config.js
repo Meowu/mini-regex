5d1edae54a261fe2ee9a29053bcba3c2868ed336
Meowu <474384902@qq.com>
2019-08-10 23:14:26 +0800

    disable ts-jest's complaint about compiler error.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
+  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
